f47c75db1e24ec5159d093d6a3d49a95fdfc09c3
Ricardo Soares <ricardo.soares@gmail.com>
2025-02-03 20:32:57 -0300

    Redis caching for orders

--- src/main/java/com/mouts/service/OrderService.java
+++ src/main/java/com/mouts/service/OrderService.java
@@ -7,7 +7,8 @@ import com.mouts.model.OrderHistory;
 import com.mouts.repository.OrderRepository;
 import com.mouts.constants.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -29,41 +30,33 @@ public class OrderService {
     }
 
     public Order salvarOrder(Order order) {
-        
         Optional<Order> orderExistente = orderRepository.findByCodigoOrder(order.getCodigoOrder());
-
         if (orderExistente.isPresent()) {
             throw new OrderDuplicadoException("Order duplicado com o código: " + order.getCodigoOrder());
         }
-
         order.setDataCriacao(new Date());
         order.setStatus(OrderStatus.RECEBIDO);
         order.calcularValorTotal();
-        
         OrderHistory orderHistory = new OrderHistory();
         orderHistory.setDataAtualizacao(order.getDataCriacao());
         orderHistory.setStatus(order.getStatus());
         order.getHistory().add(orderHistory);
-
         return orderRepository.save(order);
     }
 
+    @Cacheable(value = "ordersCache", key = "#status")
     public List<Order> listarOrdersPorStatus(String status) {
         return orderRepository.findByStatus(status);
     }
 
-    // Garante que apenas uma thread possa executar o método `atualizarStatusOrders` por vez, 
-    // impede que dados sejam alterados simultaneamente por várias threads.
-    // TODO: Logar o erro e deixar os demais itens da lista passarem
+    @CacheEvict(value = "ordersCache", allEntries = true)
     public synchronized List<Order> atualizarStatusOrders(List<Order> orders, String status) {
         for (Order order : orders) {
             Optional<Order> orderExistente = orderRepository.findByCodigoOrder(order.getCodigoOrder());
             if (orderExistente.isPresent()) {
                 orderExistente.get().setStatus(status);
-
                 OrderHistory orderHistory = new OrderHistory(new Date(), orderExistente.get().getStatus());
-                orderExistente.get().getHistory().add(orderHistory);                
-
+                orderExistente.get().getHistory().add(orderHistory);
                 orderRepository.save(orderExistente.get());
             } else {
                 throw new OrderNotFoundException("Order não encontrado com o código: " + order.getCodigoOrder());
